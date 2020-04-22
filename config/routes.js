1460324e20ac99f87149dc31f8b1eec213e45b11
Subhradip D <virtuallified@gmail.com>
2020-04-23 04:03:08 +0530

    configuration change as per the requirement of the project

--- config/routes.js
+++ config/routes.js
@@ -11,27 +11,44 @@
 module.exports.routes = {
 
   /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Make the view located at `views/homepage.ejs` your home page.            *
+   *                                                                          *
+   * (Alternatively, remove this and add an `index.html` file in your         *
+   * `assets` directory)                                                      *
+   *                                                                          *
+   ***************************************************************************/
 
-  '/': { view: 'pages/homepage' },
+  '/': {
+    view: 'pages/home'
+  },
+  // For API's (Postman)
+  /* 
+  'GET /Books': 'Books.allbooks',
+  'GET /Books/Show/:id': 'Books.onebook',
+  'POST /Books/Create': 'Books.create',
+  'PUT /Books/:id': 'Books.update',
+  'DELETE /Books/:id': 'Books.delete',
+   */
+  // For WebApp Routing
+  'GET /Books': 'Books.allbooks',
+  'GET /Books/Show/:id': 'Books.onebook',
+  'POST /Books/Create': 'Books.create',
+  'PUT /Books/:id': 'Books.update',
+  'DELETE /Books/:id': 'Books.delete',
+  'POST /': 'Books.edit'
 
 
   /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * More custom routes here...                                               *
+   * (See https://sailsjs.com/config/routes for examples.)                    *
+   *                                                                          *
+   * If a request to a URL doesn't match any of the routes in this file, it   *
+   * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+   * not match any of those, it is matched against static assets.             *
+   *                                                                          *
+   ***************************************************************************/
 
 
 };
